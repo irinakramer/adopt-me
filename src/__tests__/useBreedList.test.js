8bb2a4998c3b40f66e1e2c5b6d4c22ee8d3dba7d
Irina Kramer <irakramer@gmail.com>
2021-07-06 14:21:02 -0700

    testing api with mocks

--- src/__tests__/useBreedList.test.js
+++ src/__tests__/useBreedList.test.js
@@ -10,3 +10,29 @@ test("gives an empty list with no animal", async () => {
     expect(breedList).toHaveLength(0);
     expect(status).toBe("unloaded");
 });
+
+test ("gives back breeds with animal", async () => {
+    const breeds = [
+        "Havanese",
+        "Bichon Frise",
+        "Poodle",
+        "Maltese",
+        "Golden Retriever",
+        "Labrador",
+        "Husky",
+      ];
+
+      fetch.mockResponseOnce(JSON.stringify({
+          animal: "god",
+          breeds
+      }));
+
+      const { result, waitForNextUpdate } = renderHook(() => useBreedList("dog"));
+
+      await waitForNextUpdate();
+
+      const [breedList, status] = result.current;
+      expect(status).toBe("loaded");
+      expect(breedList).toEqual(breeds);
+
+})

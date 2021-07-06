ba6fa84b0718570403d4bf5c01b2632d35f13a96
Irina Kramer <irakramer@gmail.com>
2021-07-06 14:10:34 -0700

    testing custom hooks

---
+++ src/__tests__/useBreedList.test.js
@@ -0,0 +1,12 @@
+import { expect, test } from "@jest/globals";
+import { render } from "@testing-library/react";
+import { renderHook } from '@testing-library/react-hooks';
+import useBreedList from "../useBreedList.js";
+
+test("gives an empty list with no animal", async () => {
+    const { result } = renderHook(() => useBreedList(""))
+    const [breedList, status] = result.current;
+
+    expect(breedList).toHaveLength(0);
+    expect(status).toBe("unloaded");
+});
